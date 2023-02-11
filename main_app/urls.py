0bdbf241f2516099ede93d8abee09d6dcd8e1a9f
mar69287 <marcoa.ruiz97@gmail.com>
2023-02-11 11:25:22 -0800

    guardian detail url/view

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('guardians/',views.guardians_index, name ='guardians_index'),
+    path('guardians/<int:guardian_id>/', views.guardians_detail, name='guardians_detail'),
     path('children/create/', views.ChildCreate.as_view(), name='children_create'),
 ]
